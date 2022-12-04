7d3a60cc719d03543e6bd834cb0333f69423a55f
Marcus Venicius Nunes Biava <marcusbiava@gmail.com>
2022-12-04 15:04:54 -0300

    files

---
+++ files.go
@@ -0,0 +1,32 @@
+package gohelpers
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func readLines(path string) []string {
+	file, err := os.Open(path)
+	IfAnErrorOccursCallsLogFatal("readLines", err)
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines
+}
+
+func writeLines(lines []string, path string) {
+	file, err := os.Create(path)
+	IfAnErrorOccursCallsLogFatal("writeLines create", err)
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+	for _, line := range lines {
+		fmt.Fprintln(w, line)
+	}
+	w.Flush()
+}
---
+++ files_test.go
@@ -0,0 +1,26 @@
+package gohelpers
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+func Test_files(t *testing.T) {
+	fileName := "test_files/file1"
+	os.Remove(fileName)
+
+	data := []string{"1", "2", "3", "4"}
+
+	writeLines(data, fileName)
+
+	newData := readLines(fileName)
+
+	ok := reflect.DeepEqual(data, newData)
+
+	if !ok {
+		t.Errorf("Error in Test_files func")
+	}
+
+	os.Remove(fileName)
+}
